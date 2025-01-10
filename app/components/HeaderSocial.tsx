8278dfdad29795349a6d326e670db1f99542ebb2
3traisus <JesusNunezRamirez@3traisus.com>
2025-01-10 14:18:13 -0600

    hcanges: Headersocial finished

--- app/components/HeaderSocial.tsx
+++ app/components/HeaderSocial.tsx
@@ -1,3 +1,9 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 export function HeaderSocial() {
-  return <p>Header</p>;
+  return (
+    <div className="flex w-full lg:max-w-[1024px] justify-end">
+      <FaLinkedin className="w-[32px] h-[32px] mr-2" />
+      <FaGithub className="w-[32px] h-[32px]" />
+    </div>
+  );
 }
